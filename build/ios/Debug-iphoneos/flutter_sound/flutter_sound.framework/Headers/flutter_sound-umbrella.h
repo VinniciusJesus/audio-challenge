c8ff2005849857e65603019034cb975e88d6329c
Marcos V. Jesus Pereira <marcosv.jesuspereira@iMac-de-Seg.local>
2020-08-03 14:38:34 -0300

    backup 14:38h

---
+++ build/ios/Debug-iphoneos/flutter_sound/flutter_sound.framework/Headers/flutter_sound-umbrella.h
@@ -0,0 +1,24 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AudioRecorder.h"
+#import "Flauto.h"
+#import "FlautoManager.h"
+#import "FlutterFFmpegPlugin.h"
+#import "FlutterSoundPlayer.h"
+#import "FlutterSoundRecorder.h"
+#import "Track.h"
+#import "TrackPlayer.h"
+
+FOUNDATION_EXPORT double flutter_soundVersionNumber;
+FOUNDATION_EXPORT const unsigned char flutter_soundVersionString[];
+
